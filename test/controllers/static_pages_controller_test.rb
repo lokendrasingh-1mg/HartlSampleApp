dbe7c07bf826e2ea827194ed8f87592bfed2fee5
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-01 18:09:24 +0530

    add: direct routes to help, contact, about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,25 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = 'RoR Tutorial'
   end
 
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_url
     assert_response :success
-    assert_select "title",  @base_title
+    assert_select 'title', @base_title
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test 'should get about' do
+    get about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
 end

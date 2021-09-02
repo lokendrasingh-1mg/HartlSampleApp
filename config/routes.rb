1dee1eba3d98c3a76662ac3ce44e76c934516741
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-02 22:05:07 +0530

    fix logout: change get to delete

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
-  get '/logout', to: 'session#destroy'
+  delete '/logout', to: 'session#destroy'
   resources :users
 end

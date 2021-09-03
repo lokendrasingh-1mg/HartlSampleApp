f72f91da7be4f6800676bad1cda6b4d4499c9227
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-03 15:58:53 +0530

    update seed to be admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'Tester',
-             email: 'test@foo.com',
-             password: 'foobar',
-             password_confirmation: 'foobar')
+User.create!(
+  name: 'Tester',
+  email: 'test@foo.com',
+  password: 'foobar',
+  password_confirmation: 'foobar',
+  admin: true,
+)
 
 99.times do |n|
   name = Faker::Name.name

0636bfd84e74f59bc43f17447e27383cb1920526
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-03 16:06:12 +0530

    delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index edit update]
+  before_action :logged_in_user, only: %i[index edit update destroy]
   before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: %i[destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User Destroyed'
+    redirect_to users_url
+  end
+
   private
 
   def correct_user
@@ -56,5 +63,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end

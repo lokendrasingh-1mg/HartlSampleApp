f189336ca2456bd58e3598740209473221c894a6
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-05 00:25:41 +0530

    generate feed from list of followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where('user_id = ?', id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def self.new_token
@@ -81,6 +82,7 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
 
   private
 
